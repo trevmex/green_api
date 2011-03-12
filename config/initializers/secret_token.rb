f89d042c802ab14582fb3b7a571867d2c2f3ee1b
Trevor Menagh <trev@trevreport.org>
2011-03-12 11:25:51 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GreenApi::Application.config.secret_token = '130a31bf821364c8454a9bab61acd984f86c3719a334ca81d57fc0a3814c7fcf224f584c40e0c238d1de521150032da4392b973756e383765e33ff61976f1c26'
